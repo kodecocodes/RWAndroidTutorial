a96ed54c687ea8052d42398f09e159b0d0b4d9bf
Nishant Srivastava <nisrulz@gmail.com>
2019-12-11 23:36:06 +0100

    switch to latest test runner for Android Tests

--- app/src/androidTest/java/com/raywenderlich/android/rwandroidtutorial/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/raywenderlich/android/rwandroidtutorial/ExampleInstrumentedTest.kt
@@ -30,13 +30,11 @@
 
 package com.raywenderlich.android.rwandroidtutorial
 
-import androidx.test.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
-
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -45,10 +43,11 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
   @Test
   fun useAppContext() {
     // Context of the app under test.
-    val appContext = InstrumentationRegistry.getTargetContext()
+    val appContext = InstrumentationRegistry.getInstrumentation().targetContext
     assertEquals("com.raywenderlich.android.rwandroidtutorial", appContext.packageName)
   }
 }
